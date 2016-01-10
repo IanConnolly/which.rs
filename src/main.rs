8f738e890105fe780dfa44ca3b3b434e97226d7e
Ian Connolly <ian@scribd.com>
2016-01-09 23:31:23 -0800

    99% working there

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,62 @@
 use std::env;
 use std::fs;
 use std::path::Path;
-
+use std::convert::AsRef;
+use std::process::exit;
 
 fn main() {
     let args = env::args().collect::<Vec<_>>();
 
     if args.len() < 1 {
-        return;
+        exit(1)
     }
+
+    let mut all = false;
+    let mut bool_only = false;
+    let mut file = String::new();
+
+    for arg in args.iter().skip(1) {
+        match arg.as_ref() {
+            "-a" => all = true,
+            "-s" => bool_only = true,
+            name => file.push_str(name),
+        };
+    }
+
+    if file.is_empty() {
+        println!("empty");
+        exit(1)
+    }
+
+    let paths = paths_for_executable(file.as_ref());
+
+    if bool_only {
+        if paths.is_empty() {
+            exit(1);
+        }
+
+        exit(0);
+    }
+
+    if paths.is_empty() {
+        println!("{} not found", file);
+        exit(0);
+    }
+
+    if all {
+        for path in paths {
+            println!("{}", path);
+        }
+
+        exit(0);
+    }
+
+    println!("{}", paths[0]);
+
+}
+
+fn paths_for_executable(executable: &str) -> Vec<String> {
+    let mut vec = Vec::new();
     let path = env::var("PATH").expect("No PATH environment variable set");
     let directories = path.split(":").map(Path::new).collect::<Vec<&Path>>();
 
@@ -23,18 +71,13 @@ fn main() {
         for path in paths {
             let file = path.unwrap().path();
 
-            if let Ok(f) = fs::metadata(&file) {
-                if !f.is_file() {
-                    continue;
+            if let Some(name) = file.file_name() {
+                if executable == name.to_str().unwrap() {
+                    vec.push(file.to_str().unwrap().to_string());
                 }
-
-                if let Some(name) = file.file_name() {
-                    println!("{}", name.to_str().unwrap());
-                }
-
             }
-
         }
-
     }
+
+    vec
 }
